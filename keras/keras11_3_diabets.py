970a2cbf87bebb1213dc01043021628942297f48
soknamu <126450951+soknamu@users.noreply.github.com>
2023-03-03 19:25:46 +0900

    Update keras11_3_diabets.py

--- keras/keras11_3_diabets.py
+++ keras/keras11_3_diabets.py
@@ -16,54 +16,25 @@ from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,
-        train_size=0.7, shuffle=True, random_state=67)
-
+        train_size=0.9, shuffle=True, random_state=650874)
+#650874
 #2. 모델구성
 
 model = Sequential()
 #model.add(Dense(12,input_dim=10))
-model.add(Dense(5,input_dim=10))
-model.add(Dense(7))
-model.add(Dense(8))
-model.add(Dense(12))
-model.add(Dense(21))
-model.add(Dense(34))
-model.add(Dense(46))
-model.add(Dense(54))
-model.add(Dense(64))
-model.add(Dense(73))
-model.add(Dense(83))
-model.add(Dense(90))
-model.add(Dense(103))
-model.add(Dense(118))
-model.add(Dense(118))
-model.add(Dense(118))
-model.add(Dense(118))
-model.add(Dense(118))
-model.add(Dense(128))
-model.add(Dense(138))
-model.add(Dense(136))
-model.add(Dense(124))
-model.add(Dense(120))
-model.add(Dense(110))
-model.add(Dense(95))
-model.add(Dense(82))
-model.add(Dense(79))
-model.add(Dense(67))
-model.add(Dense(53))
-model.add(Dense(49))
-model.add(Dense(38))
-model.add(Dense(27))
-model.add(Dense(20))
-model.add(Dense(12))
-model.add(Dense(8))
-model.add(Dense(4))
+model.add(Dense(10,input_dim=10))
+model.add(Dense(30))
+model.add(Dense(50))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(30))
+model.add(Dense(5))
 model.add(Dense(1))
 
 #3. compile
 
 model.compile(loss = 'mse', optimizer = 'adam') 
-model.fit(x_train, y_train, epochs = 1650, batch_size =15)
+model.fit(x_train, y_train, epochs = 500, batch_size =8)
 
 #4. 평가,예측
 
@@ -76,5 +47,8 @@ y_predict = model.predict(x_test)
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print('r2스코어 : ', r2)
+
+#2/2 [==============================] - 0s 3ms/step
+#r2스코어 :  0.7238750746771716
 
 
