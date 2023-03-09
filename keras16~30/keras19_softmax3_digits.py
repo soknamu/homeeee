59aa5cdc3bc52005032c4a45ba7c407e3ca6db8f
soknamu <126450951+soknamu@users.noreply.github.com>
2023-03-09 20:57:09 +0900

    Update keras19_softmax3_digits.py

--- keras16~30/keras19_softmax3_digits.py
+++ keras16~30/keras19_softmax3_digits.py
@@ -26,23 +26,22 @@ print('y의 라벨값 : ', np.unique(y)) #[0 1 2 3 4 5 6 7 8 9] y의 라벨의 �
 
 ##########################이 지점에서 원핫인코딩을 한다###########################
 
-#1.텐서플로우 방법
-from tensorflow.keras.utils import to_categorical #tensorflow 빼도 가능.
-y = to_categorical(y)
-print(y.shape) #(150, 3)
+#1. tensorflow 
+# from tensorflow.keras.utils import to_categorical #tensorflow 빼도 가능.
+# y = to_categorical(y)
+# print(y.shape) #(178, 3)
 
-#2. sklearn 방법
-from sklearn.preprocessing import OneHotEncoder
-ohe = OneHotEncoder()
-y = y.reshape(-1,1)
-y = ohe.fit_transform(y).toarray()
-print(y.shape)
+#2. sklearn
+# from sklearn.preprocessing import OneHotEncoder
+# ohe = OneHotEncoder()
+# y = y.reshape(-1,1)
+# y = ohe.fit_transform(y).toarray()
+# print(y.shape)
 
-# 3.판다스 겟더미
+# 3.pandas get_dummies
 # import pandas as pd
 # y=pd.get_dummies(y)
 # print(y.shape)
-#판다스에 겟더미, sklearn 원핫인코더??
 ################################################################################
 
 x_train, x_test, y_train, y_test =  train_test_split(x,y, 
